41b2b3d89df78c2586c8edeeef169eedbfb710ae
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-23 00:37:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,6 @@ def select_series_title_with_most_human_characters
   WHERE characters.species = 'human'
   ORDER BY COUNT(*) DESC LIMIT 1"
   
-  
-  GROUP BY species ORDER BY"
 end
 
 def select_character_names_and_number_of_books_they_are_in
