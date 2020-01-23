d408ebeea9c0d04f7d10bd69bbcd73b482f46db1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-23 00:33:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series    
-  JOIN books ON books.series_id = series.id JOIN 
+  JOIN books ON books.series_id = series.id 
+  JOIN character_books ON character_books.book_id = book.id
   
   
   GROUP BY species ORDER BY"
