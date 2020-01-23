da0ea529f2a1b9320c2b10c0376b5989028234fd
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-23 00:51:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books) FROM characters 
+  "SELECT characters.name, COUNT(*) as bookzz FROM characters 
   JOIN character_books ON character_books.character_id = character.id
   ORDER BY COUNT(books)
   "
