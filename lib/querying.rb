7ace7881a87c668e2d05362b51ebecf344c9d69a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-23 00:46:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,5 +26,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books) FROM characters 
-  JOIN character_books ON character_books.character_id = character.id"
+  JOIN character_books ON character_books.character_id = character.id
+  "
 end
